053c951d5fb1f5872a47fad1e6f0b3ca6cae9565
MyeongQ <gmk0904@ajou.ac.kr>
2025-02-02 20:10:39 +0900

    fix: redirect 수정

--- src/main/java/pheonix/classconnect/backend/security/AuthController.java
+++ src/main/java/pheonix/classconnect/backend/security/AuthController.java
@@ -74,7 +74,7 @@ public class AuthController {
         redirect.addAttribute("token", token);
         redirect.addAttribute("signup", userInfo == null);
 
-        return "redirect:" + "https://" + serverDomain + ":" + serverPort + "api/v2/auth/redirect";
+        return "redirect:" + "https://" + serverDomain + ":" + serverPort + "/api/v2/auth/redirect";
     }
 
     @GetMapping("/redirect")
