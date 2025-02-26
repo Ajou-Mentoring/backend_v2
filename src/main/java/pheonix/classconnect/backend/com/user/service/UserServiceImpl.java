8a3c4dace8705e82e45b361437453ceb0b4d5e00
MyeongQ <gmk0904@ajou.ac.kr>
2025-02-26 23:26:45 +0900

    feat: 사용자 전체 조회 및 멤버 추방 API 추가

--- src/main/java/pheonix/classconnect/backend/com/user/service/UserServiceImpl.java
+++ src/main/java/pheonix/classconnect/backend/com/user/service/UserServiceImpl.java
@@ -2,14 +2,16 @@ package pheonix.classconnect.backend.com.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import pheonix.classconnect.backend.com.attachment.constants.AttachmentDomainType;
-import pheonix.classconnect.backend.com.attachment.entity.FileEntity;
-import pheonix.classconnect.backend.com.attachment.model.File;
 import pheonix.classconnect.backend.com.attachment.service.FileStorage;
 import pheonix.classconnect.backend.com.auth.constant.AuthorityCode;
 import pheonix.classconnect.backend.com.auth.entity.AuthorityEntity;
 import pheonix.classconnect.backend.com.auth.repository.AuthorityRepository;
+import pheonix.classconnect.backend.com.common.model.Paged;
 import pheonix.classconnect.backend.com.department.entity.DepartmentEntity;
 import pheonix.classconnect.backend.com.department.repository.DepartmentRepository;
 import pheonix.classconnect.backend.com.user.constant.UserActiveStatus;
@@ -87,5 +89,59 @@ public class UserServiceImpl implements UserService {
 
         return UserDTO.User.fromEntity(userRepository.findById(id)
                 .orElseThrow(() -> new MainApplicationException(ErrorCode.USER_NOT_FOUND, "유저 정보가 없습니다.")));
+    }
+
+    @Override
+    public Paged<UserDTO.User> findUsersByName(String name, int page, int size) {
+        log.info("유저 페이지 조회 : 이름");
+        Pageable pageable = PageRequest.of(page, size, Sort.by(List.of(Sort.Order.asc("name"))));
+        Page<UserEntity> users = userRepository.findAllByNameContainsIgnoreCase(name, pageable);
+
+        return Paged.<UserDTO.User>builder()
+                .currentPage(users.getNumber())
+                .size(users.getSize())
+                .numberOfElements(users.getNumberOfElements())
+                .totalPages(users.getTotalPages())
+                .totalElements(users.getTotalElements())
+                .items(users.getContent().stream()
+                        .map(UserDTO.User::fromEntity)
+                        .toList())
+                .build();
+    }
+
+    @Override
+    public Paged<UserDTO.User> findUsersByStudentNo(String no, int page, int size) {
+        log.info("유저 페이지 조회 : 학번");
+        Pageable pageable = PageRequest.of(page, size, Sort.by(List.of(Sort.Order.asc("name"))));
+        Page<UserEntity> users = userRepository.findAllByStudentNoContains(no, pageable);
+
+        return Paged.<UserDTO.User>builder()
+                .currentPage(users.getNumber())
+                .size(users.getSize())
+                .numberOfElements(users.getNumberOfElements())
+                .totalPages(users.getTotalPages())
+                .totalElements(users.getTotalElements())
+                .items(users.getContent().stream()
+                        .map(UserDTO.User::fromEntity)
+                        .toList())
+                .build();
+    }
+
+    @Override
+    public Paged<UserDTO.User> findUsers(int page, int size) {
+        log.info("유저 페이지 조회");
+        Pageable pageable = PageRequest.of(page, size, Sort.by(List.of(Sort.Order.asc("name"))));
+        Page<UserEntity> users = userRepository.findAll(pageable);
+
+        return Paged.<UserDTO.User>builder()
+                .currentPage(users.getNumber())
+                .size(users.getSize())
+                .numberOfElements(users.getNumberOfElements())
+                .totalPages(users.getTotalPages())
+                .totalElements(users.getTotalElements())
+                .items(users.getContent().stream()
+                        .map(UserDTO.User::fromEntity)
+                        .toList())
+                .build();
     }
 }
