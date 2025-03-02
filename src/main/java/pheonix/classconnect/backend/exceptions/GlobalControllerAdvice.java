7f9f8ba0035225a977fe8b6750fd9db1d3507244
MyeongQ <gmk0904@ajou.ac.kr>
2025-03-02 15:45:01 +0900

    fix: @Valid 관련 에러핸들러 추가

--- src/main/java/pheonix/classconnect/backend/exceptions/GlobalControllerAdvice.java
+++ src/main/java/pheonix/classconnect/backend/exceptions/GlobalControllerAdvice.java
@@ -1,6 +1,7 @@
 package pheonix.classconnect.backend.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import pheonix.classconnect.backend.com.common.model.Response;
@@ -17,9 +18,17 @@ public class GlobalControllerAdvice {
         return Response.error(e.getErrorCode(), e.getMessage());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Response<String> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        log.error(e.getMessage());
+
+        return Response.error(ErrorCode.BACK_INVALID_PARAMETER, "API 파라미터 에러입니다.");
+    }
+
+
     @ExceptionHandler(Exception.class)
     public Response<String> unhandledExceptionHandler(Exception e) {
-        log.error(e.getMessage());
+        log.error("{} => {}", e.getClass(), e.getMessage());
         return Response.error(ErrorCode.SYS_UNCAUGHT_ERROR, "확인되지 않은 에러입니다. IT 담당자에게 문의하세요.");
     }
 }
